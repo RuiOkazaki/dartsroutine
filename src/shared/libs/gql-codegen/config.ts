a5f02c022ed037d2ce9cd33cb456a6bc5d29ecc3
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-05-06 00:39:12 +0900

    chore: `fetch`を使用するために`typescript-generic-sdk`に変更した

--- src/shared/libs/gql-codegen/config.ts
+++ src/shared/libs/gql-codegen/config.ts
@@ -12,10 +12,42 @@ const config: CodegenConfig = {
       },
     },
   ],
+  documents: ['src/**/*.tsx', 'src/**/*.ts', 'src/**/*.graphql'],
   generates: {
     'src/shared/libs/gql-codegen/generated/': {
       preset: 'client',
-      plugins: ['typescript-urql'],
+      config: {
+        gqlTagName: 'graphql',
+        fragmentMasking: true,
+        avoidOptionals: {
+          field: true,
+          inputValue: false,
+          object: true,
+          defaultValue: false,
+        },
+        skipTypename: true,
+        immutableTypes: true,
+        defaultScalarType: 'unknown',
+        enumsAsTypes: true,
+        useTypeImports: true,
+      },
+    },
+    'src/shared/libs/gql-codegen/generated/sdk.ts': {
+      plugins: [
+        {
+          add: {
+            content: "import * as Type from './graphql.ts'",
+          },
+        },
+        {
+          'typescript-generic-sdk': {
+            useTypeImports: true,
+            documentMode: 'external',
+            importDocumentNodeExternallyFrom: './graphql.ts',
+            importOperationTypesFrom: 'Type',
+          },
+        },
+      ],
     },
   },
 };
