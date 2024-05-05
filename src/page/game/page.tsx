960974966ed7aa4b79074c96444b36d436aaccef
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-05-06 05:08:48 +0900

    feat: ゲーム選択uiを実装した

--- src/page/game/page.tsx
+++ src/page/game/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Button } from '@/shared/ui/button';
+import { Typography } from '@/shared/ui/typography';
 import { MenuIcon, SettingsIcon, UserIcon } from 'lucide-react';
 import './style.css';
 
@@ -19,7 +22,46 @@ export default async function Game() {
           </Button>
         </div>
       </header>
-      <main className='p-4'>hoge</main>
+      <main className='h-full p-4'>
+        <div className='flex h-full items-center justify-center gap-8'>
+          <div className='h-3/4'>
+            <Typography
+              className='grid h-full w-36 place-items-center border border-red-500 text-red-500 uppercase'
+              variant='body-m'
+              weight='bold'
+              asChild
+            >
+              <Button
+                variant='unstyle'
+                onClick={() => {
+                  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+                  console.log('onClick!');
+                }}
+              >
+                Target Bull
+              </Button>
+            </Typography>
+          </div>
+          <div className='h-3/4'>
+            <Typography
+              className='grid h-full w-36 place-items-center border border-blue-500 text-blue-500 uppercase'
+              variant='body-m'
+              weight='bold'
+              asChild
+            >
+              <Button
+                variant='unstyle'
+                onClick={() => {
+                  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+                  console.log('onClick!');
+                }}
+              >
+                CR Number
+              </Button>
+            </Typography>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
