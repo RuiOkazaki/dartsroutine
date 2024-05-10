f4288cd5bb4782e7fb07fbd7c1bccd758e6ce487
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-05-11 01:26:22 +0900

    feat: ターゲットブルのゲームモード選択画面を実装

--- src/page/game-configure/index.tsx
+++ src/page/game-configure/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { Button } from '@/shared/ui/button';
+import { Tabs, TabsList, TabsTrigger } from '@/shared/ui/tabs';
+import { Typography } from '@/shared/ui/typography';
 import { GameHeader } from '@/widget/game-header';
 import { HomeIcon, MenuIcon } from 'lucide-react';
 
@@ -30,8 +32,36 @@ export default async function Game() {
         }
       />
       <main className='h-full p-4'>
-        <div className='flex h-full items-center justify-center gap-8'>
-          ほげ
+        <div className='flex h-full flex-col items-center justify-center gap-7'>
+          <div className='flex items-center gap-7'>
+            <span className='block aspect-video h-24 bg-gray-300' />
+            <div className='flex w-72 flex-col gap-2'>
+              <Typography variant='body-l'>
+                ゲームの難易度を選択してください
+              </Typography>
+              <span className='border-b' />
+              <Typography>
+                ターゲットにn（5,10,30,50,100）回
+                <br />
+                刺さるまで狙い続ける
+              </Typography>
+            </div>
+          </div>
+          <Tabs defaultValue='5'>
+            <TabsList>
+              <TabsTrigger value='5'>5</TabsTrigger>
+              <TabsTrigger value='10'>10</TabsTrigger>
+              <TabsTrigger value='30'>30</TabsTrigger>
+              <TabsTrigger value='50'>50</TabsTrigger>
+              <TabsTrigger value='100'>100</TabsTrigger>
+            </TabsList>
+          </Tabs>
+          <Button
+            variant='outline'
+            className='w-80 border-red-500 text-red-500 hover:bg-red-500 hover:text-white'
+          >
+            ゲームを開始する
+          </Button>
         </div>
       </main>
     </div>
--- src/shared/ui/tabs.tsx
+++ src/shared/ui/tabs.tsx
@@ -4,6 +4,7 @@ import * as TabsPrimitive from '@radix-ui/react-tabs';
 import * as React from 'react';
 
 import { cn } from '@/shared/libs/shadcn/utils';
+import { Typography } from './typography';
 
 const Tabs = TabsPrimitive.Root;
 
@@ -14,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground',
+      'inline-flex h-10 items-center justify-center rounded-md bg-transparent p-1 text-red-500',
       className,
     )}
     {...props}
@@ -29,27 +30,14 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 font-medium text-sm ring-offset-background transition-all disabled:pointer-events-none data-[state=active]:bg-background data-[state=active]:text-foreground disabled:opacity-50 data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      'inline-flex w-24 items-center justify-center whitespace-nowrap rounded-sm border-red-500 border-t border-b border-l px-3 py-1.5 font-medium text-sm ring-offset-red-500 transition-all disabled:pointer-events-none last:border-r data-[state=active]:bg-red-200 data-[state=active]:text-red-500 disabled:opacity-50 data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
       className,
     )}
     {...props}
-  />
+  >
+    <Typography variant='body-m'>{props.children}</Typography>
+  </TabsPrimitive.Trigger>
 ));
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-      className,
-    )}
-    {...props}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
-
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export { Tabs, TabsList, TabsTrigger };
