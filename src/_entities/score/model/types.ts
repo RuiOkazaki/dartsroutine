cf66a10f6568b452b311f66ee8bd97d35336c926
RuiOkazaki <r_okazaki@yumemi.co.jp>
2024-06-03 01:53:53 +0900

    fix: リレーションが型に反映されたため、フロントで扱う型からは除くように変更

--- src/_entities/score/model/types.ts
+++ src/_entities/score/model/types.ts
@@ -1,6 +1,7 @@
-import type { Darts_Positions } from '@/_shared/lib/gql-codegen/api/generated/graphql';
+import type { Darts_Positions_Max_Fields } from '@/_shared/lib/gql-codegen/api/generated/graphql';
 
-export type DartsPosition = Darts_Positions;
+// MEMO: リレーションを取り除いた型定義を使用している
+export type DartsPosition = Darts_Positions_Max_Fields;
 export type DartsHitHistory = DartsPosition | null;
 export type DartsRound = [DartsHitHistory?, DartsHitHistory?, DartsHitHistory?];
 export type DartsRoundsHistory = Array<DartsRound>;
